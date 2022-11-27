5b416ae2490b45637402009ba4326d1d33a92524
thebigG <yunior.eury@gmail.com>
2022-11-27 12:59:39 -0600

    -Add readall

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hell world from GPIO code!");
+// Copyright (c) 2018 The rust-gpio-cdev Project Developers.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+
+use gpio_cdev::{Chip, LineRequestFlags};
+use quicli::prelude::*;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Cli {
+    /// The gpiochip device (e.g. /dev/gpiochip0)
+    chip: String,
 }
+
+fn main() -> CliResult {
+    let args = Cli::from_args();
+    readall(args).or_else(|e| {
+        error!("{:?}", e);
+        Ok(())
+    })
+}
+
+fn readall(args: Cli) -> std::result::Result<(), gpio_cdev::Error> {
+    let mut chip = Chip::new(args.chip)?;
+    let ini_vals = vec![0; chip.num_lines() as usize];
+    let handle = chip
+        .get_all_lines()?
+        .request(LineRequestFlags::INPUT, &ini_vals, "readall")?;
+    println!("Values: {:?}", handle.get_values()?);
+
+    Ok(())
+}
+
